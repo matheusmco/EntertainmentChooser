1f072af7d6535bec70f505dd048ea0048c4d995e
Matheus Oliveira <matheus0mco@gmail.com>
2021-01-28 18:40:06 -0300

    feat:added Context

--- ChooseEntertainmentItem.Infra/Context.cs
+++ ChooseEntertainmentItem.Infra/Context.cs
@@ -3,17 +3,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ChooseEntertainmentItem.Infra.EFContexts
 {
-    // TODO: create a base class
-    // TODO: override using a new one
     public abstract class BaseContext : DbContext
     {
+        protected virtual string ConnectionString { get; set; }
+
         public DbSet<BacklogItem> BacklogItems { get; set; }
         public DbSet<DoneItem> DoneItems { get; set; }
     }
 
     public class SqliteContext : BaseContext
     {
+
+        public SqliteContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Data Source=items.db"); // TODO: pass data source in config
+            => options.UseSqlite(ConnectionString);
+            // => options.UseSqlite("Data Source=/home/matheus/repos/ChooseEntertainmentItem/items.db");
     }
 }
